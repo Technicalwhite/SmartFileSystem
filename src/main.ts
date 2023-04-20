551d5c27ac1b32a7b9c4237fbb725b1ae3710675
QAQ <2234558846@qq.com>
2023-04-20 19:03:19 +0800

    测试一次提交远程

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@
  * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
  * @Date: 2023-03-31 01:11:55
  * @LastEditors: QAQ 2234558846@qq.com
- * @LastEditTime: 2023-04-20 18:34:27
+ * @LastEditTime: 2023-04-20 19:02:54
  * @FilePath: \SmartFileSystem\src\main.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -98,7 +98,7 @@ app.get('/q', (req, res) => {
             }
         }
     }
-    // let sssdb = mongoose.useDb('mydb');
+    let sssdb = mongoose.useDb('mydb');
     const spl = {...mongoose}
     console.log('请求的/q',spl)
     let cache:Array<any> = [];
